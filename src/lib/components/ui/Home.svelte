7b9f21f7776ce1683145c57476e2b732fdd1a864
Ronald Otieno <ronnlats@gmail.com>
2023-10-10 18:20:08 +0000

    beginning of desing

--- src/lib/components/ui/Home.svelte
+++ src/lib/components/ui/Home.svelte
@@ -3,55 +3,36 @@
 </script>
 
 <div
-  class="relative flex flex-col-reverse font-[Ubuntu] w-full py-16 lg:pt-0 lg:flex-col lg:pb-0"
+  class="relative flex flex-col font-[Ubuntu] w-full  lg:pt-0 lg:flex-col lg:pb-0"
 >
-  <div
-    class="inset-y-0 top-0 right-0 z-0 w-full max-w-xl px-4 mx-auto md:px-0 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-7/12 lg:max-w-full lg:absolute xl:px-0"
-  >
-    <svg
-      class="absolute left-0 hidden h-full text-gray-400 transform -translate-x-1/2 lg:block"
-      viewBox="0 0 100 100"
-      fill="currentColor"
-      preserveAspectRatio="none slice"
-    >
-      <path d="M50 0H100L50 100H0L50 0Z" />
-    </svg>
-    <img
-      class="object-cover w-full bg-myColor-100 rounded shadow-sm h-96 lg:rounded-none lg:shadow-none md:h-auto lg:h-full"
-      src="/lat2-smaller.webp"
-      alt=""
-    />
+  <div class=" pb-20 bg-myColor-600 min-h-max  w-full">
+    <h1 class="text-5xl font-[Maven]">Welcome to Nyamninia High School</h1>
+    <p class="text-4xl">The People For Tomorrow</p>
   </div>
-  <div
-    class="relative flex flex-col items-start w-full max-w-xl px-4 mx-auto md:px-0 lg:px-8 lg:max-w-screen-xl"
-  >
-    <div class="mb-16 lg:my-40 lg:max-w-lg lg:pr-5">
-      <p
-        class="inline-block w-full py-px mb-4 text-xs font-[Play] font-semibold tracking-wider uppercase"
-      >
-        who am i?
-      </p>
-      <h2
-        class="mb-5 font-[Maven] text-4xl font-extrabold first-letter:text-6xl first-letter:text-myColor-600 tracking-widest text-gray-900 sm:text-4xl sm:leading-none"
-      >
-        Building intelligent sytems,<br class="hidden md:block" />
-        <span class="inline-block text-deep-violet-accent-400"
-          >one line at a time.</span
-        >
-      </h2>
-      <div
-        class="flex-row space-y-2 pr-5 mb-5 text-base text-gray-700 md:text-lg"
-      >
-        <p>
-          Hello! I'm Ronald Otieno, a software developer specializing in web
-          development, data visualization, and machine learning.
+    <div class="flex flex-col  gap-2 md:flex-row   h-32   mx-2   ">
+      <div class=" w-full bg-myColor-200 p-2">
+        <p class="pt-0">Mission</p>
+        <div class="bg-transparent w-1/4 border-b-black border " />
+        <p class="text-sm">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic iusto qui
+          facilis nostrum dolorem quo.
         </p>
-        <p>
-          With expertise in dynamic web applications and a focus on D3.js for
-          captivating visualizations, I'm excited to collaborate on impactful
-          projects. Let's make a difference together!
+      </div>
+      <div class="bg-myColor-400 w-full p-2">
+        <p class="pt-0">Vision</p>
+        <div class=" w-1/4 border-b-black border " />
+        <p class="text-sm">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic iusto qui
+          facilis nostrum dolorem quo.
+        </p>
+      </div>
+      <div class="w-full bg-myColor-200 p-2">
+        <p class="pt-0">Goals</p>
+        <div class="bg-transparent w-1/4 border-t-black border " />
+        <p class="text-sm">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic iusto qui
+          facilis nostrum dolorem quo.
         </p>
       </div>
     </div>
-  </div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,10 @@
 <script>
   import Home from '$lib/components/ui/Home.svelte';
-  import Portofolio from '$lib/components/Portofolio.svelte';
-  import Tools from '$lib/components/Tools.svelte';
-  import DailyStoic from '$lib/components/DailyStoic.svelte';
   export let data;
 
-  const { quote } = data;
+  const {} = data;
 </script>
 
 <div class="min-h-screen font-[Ubuntu] bg-myColor-50 px-2 lg:px-4">
   <Home />
-  <DailyStoic {quote} />
-  <Portofolio />
-  <Tools />
 </div>
