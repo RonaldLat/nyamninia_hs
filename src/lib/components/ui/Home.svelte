501bb8ee29bb2893f67860e5d15938fca5fc8efa
Ronald Otieno <ronnlats@gmail.com>
2023-10-11 11:00:31 +0300

    d

--- src/lib/components/ui/Home.svelte
+++ src/lib/components/ui/Home.svelte
@@ -6,7 +6,7 @@
 	<div class="dark:bg-violet-400">
 		<div class="container flex flex-col items-center px-4 py-16 pb-24 mx-auto text-center lg:pb-56 md:py-32 md:px-10 lg:px-32 dark:text-gray-900">
 			<h1 class="text-5xl font-bold leadi sm:text-6xl xl:max-w-3xl dark:text-gray-900">Provident blanditiis cum exercitationem</h1>
-			<p class="mt-6 mb-8 text-lg sm:mb-12 xl:max-w-3xl dark:text-gray-900">Cupiditate minima voluptate temporibus quia? Architecto beatae esse ab amet vero eaque explicabo!</p>
+			<p class="mt-6 mb-8 text-lg sm:mb-12 xl:max-w-3xl dark:text-gray-900">nyamninia secondary school</p>
 			<div class="flex flex-wrap justify-center">
 				<button type="button" class="px-8 py-3 m-2 text-lg font-semibold rounded dark:bg-gray-800 dark:text-gray-50">Get started</button>
 				<button type="button" class="px-8 py-3 m-2 text-lg border rounded dark:border-gray-700 dark:text-gray-900">Learn more</button>
