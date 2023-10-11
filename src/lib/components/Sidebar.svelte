69b0cf1f9d798c813c5accb886325fd80a1f89e2
Ronald Otieno <ronnlats@gmail.com>
2023-10-11 10:22:55 +0300

    school calendar

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -10,12 +10,10 @@
   <aside class=" w-full h-full border-r-2 text-gray-100">
     <nav class="p-12 text-xl">
       <a class="block" class:text-white={(route = '/')} href="/">Home</a>
-      <a class="block" href="/about">About</a>
-      <a class="block" href="#contact">Contact</a>
-      <a class="block" href="/projects">Projects</a>
-      <a class="block" href="/d3">D3</a>
-      <a class="block" href="/blog">Blog</a>
-      <a class="block" href="/dsa">DSA</a>
+      <a class="block" href="/about">About Us</a>
+      <a class="block" href="/contact">Contact Us</a>
+      <a class="block" href="/calendar">School Calendar</a>
+      
     </nav>
   </aside>
 </div>
