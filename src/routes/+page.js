e67fb739e7a3f542bc1cb316818c8eea5205cd63
Ronald Otieno <ronnlats@gamail.com>
2023-10-12 09:24:24 +0300

    typo fix

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,8 +1,5 @@
 export const load = async ({ fetch }) => {
-  const response = await fetch(`https://stoic-quotes.com/api/quote`);
-  const quote = await response.json();
 
   return {
-    quote
   };
 };
